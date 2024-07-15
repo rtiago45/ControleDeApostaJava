f31a3d62b7956a5057fe9cebfc97f1ea2af66eb8
Mateus Ascacibas Da Silva <mateus.asilva2001@hotmail.com>
2024-07-15 12:45:11 -0300

    first commit

---
+++ betManager/src/main/java/com/BetManager/betManager/BetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.BetManager.betManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetManagerApplication.class, args);
+	}
+
+}
