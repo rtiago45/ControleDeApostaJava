5f0c4b465ef4a9d522a57a93936732405ea037f2
Mateus Ascacibas Da Silva <mateus.asilva2001@hotmail.com>
2024-07-16 16:06:03 -0300

    Add new method: deleteBet by ID and insertMultiple (both with tests class)

--- betManager/src/main/java/controller/BetController.java
+++ betManager/src/main/java/controller/BetController.java
@@ -1,13 +1,20 @@
 package controller;
 
-import model.BetVO;
-import repository.BetRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import model.BetVO;
+import model.MultipleBetVO;
+import repository.BetRepository;
 
 @RestController
 @RequestMapping("/bets")
@@ -20,4 +27,25 @@ public class BetController {
     public List<BetVO> getAllBets() {
         return betRepository.findAll();
     }
+    
+    @PostMapping("/insertMultiple")
+    public ResponseEntity<String> insertMultiple(@RequestBody MultipleBetVO multipleBetVO){
+    	try {
+    		betRepository.insert(multipleBetVO);
+    		return new ResponseEntity<String>("Aposta criada com sucesso!", HttpStatus.OK);
+    	} catch (Exception e) {
+    		return new ResponseEntity<>("Erro ao tentar inserir aposta multipla.", HttpStatus.BAD_REQUEST);
+    	}
+    }
+    
+    @PostMapping("/deleteBet/{id}")
+    public ResponseEntity<String> deleteBet(@PathVariable String id) {
+        try {
+            betRepository.deleteById(id);
+            return new ResponseEntity<>("Aposta deletada com sucesso!", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao tentar deletar aposta.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
