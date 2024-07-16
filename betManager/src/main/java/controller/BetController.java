bc0e32d7ccdeb0bc5c3191bd3a09a6b7a64fa42f
Tiago Rodrigues <seu.email@example.com>
2024-07-16 16:29:01 -0300

    Adicionando endpoint de criar aposta Simples

--- betManager/src/main/java/controller/BetController.java
+++ betManager/src/main/java/controller/BetController.java
@@ -1,11 +1,11 @@
 package controller;
 
 import model.BetVO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import repository.BetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -20,4 +20,16 @@ public class BetController {
     public List<BetVO> getAllBets() {
         return betRepository.findAll();
     }
+
+    @PostMapping("/insertSimpleBet")
+    public ResponseEntity<String> insertBetSimpleBet(@RequestBody BetVO bet) {
+        try {
+            BetVO savedBet = betRepository.save(bet);
+            return new ResponseEntity<String>("Aposta simples criada com sucesso!", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao criar aposta simples: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
 }
