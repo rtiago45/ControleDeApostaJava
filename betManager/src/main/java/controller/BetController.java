76c747ac5e3e8a3cd0a80819396f5d446670ee2f
Mateus Ascacibas Da Silva <mateus.asilva2001@hotmail.com>
2024-07-17 17:22:13 -0300

    Criação de web config

--- betManager/src/main/java/controller/BetController.java
+++ betManager/src/main/java/controller/BetController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import repository.MultipleBetRepository;
 
 @RestController
 @RequestMapping("/bets")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BetController {
 
 	@Autowired
